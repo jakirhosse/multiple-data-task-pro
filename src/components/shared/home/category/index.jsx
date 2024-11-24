03c2a41850892b44dcb2f394a1fbed94ddb90849
unknown <jakirhossain45951@gmail.com>
2024-11-24 13:45:04 +0600

    categor folder add

---
+++ src/components/shared/home/category/index.jsx
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
+import CategoryMenu from "../categoryMenu";
+
+const Category = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.shope.com.bd/api/v1/public/hero-categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log({ categories });
+
+  return (
+    <div>
+      <div className="-mt-[20px] absolute top-0 z-50">
+        <CategoryMenu className="w-full bg-white" categories={categories} />
+      </div>
+    </div>
+  );
+};
+
+export default Category;
